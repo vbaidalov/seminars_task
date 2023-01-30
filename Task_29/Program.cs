e4556e27af2b711b0cf210fd3b3c79a996a4d23a
baidalov <vsb-posst@yande.ru>
2023-01-30 15:16:30 +0300

    Task29

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов* и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// *В заданном пользльзователем диапазоне
+
+var rand = new Random();
+Console.WriteLine("Введите диапазон от 0 до: ");
+int a = int.Parse(Console.ReadLine()??"");
+int[] array = new int[8];
+Console.Write("["); // первая скобка
+for (int i = 0; i < 8; i++)
+{
+    Console.Write($"{rand.Next(0, a)}, "); // рандомный генератор от 0 до "a"
+}
+Console.Write("\b\b"); // обрезание пробела и последней запятой
+Console.WriteLine("]"); // последняя скобка
