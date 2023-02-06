91a6b51a387a83f0e9a6e7fbd6a74da63c0694d8
Baidalov <vsb-pots@yandex.ru>
2023-02-06 20:31:36 +0300

    Task47

---
+++ Task_47/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. 
+// m = 3, n = 4. 
+// 0,5 7 -2 -0,2 
+// 1 -3,3 8 -9,9 
+// 8 7,8 -7,1 9
+
+Console.Write("Задайте размер массива. Количество строк: "); 
+int m = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Задайте размер массива. Количество столбцов: "); 
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+FillArray(array); // вызовы функции array должны быть после создания самого массива. Это же так логично) иначе, ДО этого их не существует. 
+PrintArray(array);
+
+void FillArray(double[,] array) 
+{ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{ 
+    for (int j = 0; j < array.GetLength(1); j++) 
+    { 
+        array[i, j] = (new Random().NextDouble()*10 -5); // умножаем на 10, что бы появилась цифра перед запятой и отнимаем 5, что бы получить отрицательные числа 
+    } 
+} 
+}
+
+void PrintArray(double[,] array) 
+{ 
+    for (int i = 0; i < array.GetLength(0); i++) 
+    { 
+        
+        for (int j = 0; j < array.GetLength(1); j++) 
+        { 
+            // Console.Write($"{array[i, j]} "); 
+            Console.Write($"{Math.Round(array[i, j], 2)} "); 
+        } 
+        Console.WriteLine(); 
+    } 
+}
+
+// Console.WriteLine(Math.Round(num,2)); 
+// Console.Write("\t" + Math.Round(a[i, j], 2));
---
+++ Task_50/Program.cs
@@ -0,0 +1,67 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет. 
+//Например, задан массив: 
+//1 4 7 2 
+//5 9 2 3 
+//8 4 2 4 
+//1 7 -> такого числа в массиве нет 
+//1 1 -> 9
+
+Console.Write("Введите позицию искомого элемента. Строку и столбец церез пробел: "); 
+string[] str = Console.ReadLine().Split(' '); // заведение элементов в массив из строки 
+int line = Convert.ToInt32(str[0]); 
+int column = Convert.ToInt32(str[1]);
+
+int[,] FillArray() 
+{ 
+int[,] array = new int[5, 4]; 
+for (int i = 0; i < array.GetLength(0); i++) 
+{ 
+    for (int j = 0; j < array.GetLength(1); j++) 
+    { 
+        array[i, j] = new Random().Next(0, 10);  
+    } 
+     
+} 
+return array; 
+}
+
+int ElementSearch(int[,] array) 
+{ 
+int count = 0; 
+for (int i = 0; i < array.GetLength(0); i++) 
+{ 
+    for (int j = 0; j < array.GetLength(1); j++) 
+    { 
+        if (i == line && j == column) count = array[i, j]; 
+        //else Console.WriteLine($"{line} {column} -> такого числа в массиве нет"); 
+     
+    } 
+     
+} 
+return count; 
+}
+
+void PrintArray(int[,] array) 
+{ 
+    for (int i = 0; i < array.GetLength(0); i++) 
+    { 
+        
+        for (int j = 0; j < array.GetLength(1); j++) 
+        { 
+            Console.Write($"{array[i, j], 2} "); 
+        } 
+        Console.WriteLine(); 
+    } 
+}
+
+void PrintCount(int count) 
+{ 
+    if (count == 0) Console.WriteLine($"{line} {column} -> такого числа в массиве нет"); 
+    else Console.WriteLine($"{line} {column} -> {count}"); 
+}
+
+int[,] array = FillArray(); 
+PrintArray(array); 
+Console.WriteLine(); 
+int count = ElementSearch(array); 
+PrintCount(count);
